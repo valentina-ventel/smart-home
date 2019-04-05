a47556689501d49c143cbb5ea06bdcc4c13a00aa
Valentina <valentina.vali40@yahoo.com>
2019-04-05 13:59:17 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Valentina Vențel on 05/04/2019.
+//  Copyright © 2019 Valentina Vențel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
